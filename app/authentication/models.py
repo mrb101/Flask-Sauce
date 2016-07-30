d380e30eab89f65a97d809a1c6a2e5de3883c257
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-30 09:17:01 +0000

    Adds User and Role models to Authentication package

---
+++ app/authentication/models.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.login import UserMixin
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), nullable=False, unique=True, index=True)
+    password_hash = db.Column(db.String(255))
+    created_at = db.Column(db.Datetime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return self.username
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
