25bbda221125ac4561d00013dcc86ddd7914dea5
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-30 09:50:46 +0000

    adds authentication views and body of the templates

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,1 +1,11 @@
+from flask import render_template
+from . import authentication
 
+@authentication.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('authentication/login.html')
+
+
+@authentication.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('authentication/register.html')
