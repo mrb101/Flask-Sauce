669c80eaff8d50a1c4261fa0c32986e7210d3eb8
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-30 08:55:05 +0000

    adds main routes and setup base template

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,13 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
     return render_template('main/index.html')
+
+
+@main.route('/', methods=['GET'])
+def about():
+    return render_template('main/about.html')
+
+
+@main.route('/', methods=['GET'])
+def contact():
+    return render_template('main/contact.html')
