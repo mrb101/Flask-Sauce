a5447f5ba005cf908b2fb29cd9e979a10d8be6b8
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-31 07:53:08 +0000

    adds empty logout function

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from . import authentication
 
 @authentication.route('/login', methods=['GET', 'POST'])
@@ -9,3 +9,8 @@ def login():
 @authentication.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('authentication/register.html')
+
+
+@authentication.route('/logout', methods=['GET'])
+def logout():
+    return redirect(url_for('authentication.login'))
