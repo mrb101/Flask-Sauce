375ea131aa5d2397ab8d870f9ebedfac8a8ec6a8
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-31 15:02:46 +0000

    fixes the config and adds a commandline to add a new user

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app, db
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.environ.get('FLASK_CONFIG', 'default'))
 manager = Manager(app)
 migrate = Migrate(app, db)
 
@@ -14,6 +14,24 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
+
+from app.authentication.models import User
+
+@manager.command
+def adduser(email, username):
+    """Register a new User"""
+    from getpass import getpass
+    name = raw_input('Full Name: ')
+    password = getpass()
+    password2 = getpass(prompt='Confirm: ')
+    if password != password2:
+        import sys
+        sys.exit('Error: Password do not match')
+    db.create_all()
+    user = User(name=name, email=email, username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    print('User {0} was successfully created'.format(username))
 
 if __name__ == '__main__':
     manager.run()
