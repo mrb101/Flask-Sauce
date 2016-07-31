9fd95fb9cdcea74c8bcfbcffa0a3b8f3a6dec8d4
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-31 09:18:17 +0000

    creats login and registeration forms

---
+++ app/authentication/forms.py
@@ -0,0 +1,16 @@
+from flask.ext.wtf import Form
+from wtforms import StringField PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email
+
+
+class RegisterForm(Form):
+    name = StringField('Full Name', validators=[Required(), Length(1, 255)])
+    email = StringField('Email',
+        validators=[Required(), Length(1, 255), Email()])
+    username = StringField('User Name', validators=[Required(), Length(1, 64)])
+
+
+class LoginForm(Form):
+    username = StringField('User Name', validators=[Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[required()])
+    submit = SubmitField('Sign In')
