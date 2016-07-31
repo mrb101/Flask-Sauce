2a8f3f71efbe5c686f89919bb75d9eaba36db2cd
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-31 15:05:52 +0000

    adds login and logout views and a login template

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,9 +1,30 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, request
+from sqlalchemy.orm.exc import NoResultFound
 from . import authentication
 
-@authentication.route('/login', methods=['GET', 'POST'])
+from .forms import LoginForm
+from .models import User
+from flask.ext.login import (
+    login_user,
+    current_user,
+    login_required,
+    logout_user
+    )
+
+
+@authentication.route('/login', methods=('GET', 'POST'))
 def login():
-    return render_template('authentication/login.html')
+    form = LoginForm()
+    if request.method == 'POST':
+        password = form.password.data
+        username = form.username.data
+        user = User.query.filter_by(username=username).first()
+        if user is not None and user.verify_password(password) == True:
+            flash('Welcome to Sauce')
+            return redirect(url_for('authentication.register'))
+        flash('Invalid Username or Password!')
+        return redirect(url_for('authentication.login'))
+    return render_template('authentication/login.html', form=form)
 
 
 @authentication.route('/register', methods=['GET', 'POST'])
@@ -13,4 +34,6 @@ def register():
 
 @authentication.route('/logout', methods=['GET'])
 def logout():
+    logout_user()
+    flash('You have logged out!')
     return redirect(url_for('authentication.login'))
